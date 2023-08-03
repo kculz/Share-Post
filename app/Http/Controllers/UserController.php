9831640252fd0f262dba7e9f7284f16b6f03ad6b
kculz <munyamakudzai095@gmail.com>
2023-08-03 20:34:02 +0200

    REVERT

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,12 +28,8 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        $createdUser = User::query()->create([
-            'name' => $request->name,
-            'email' => $request->email
-        ]);
         return new JsonResponse([
-            "data" => $createdUser
+            "data" => "store"
         ]);
     }
 
